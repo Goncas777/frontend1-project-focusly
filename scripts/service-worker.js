cbcd04a729007e8760537fbb339f43896f6b4273
Goncas777 <123480612+Goncas777@users.noreply.github.com>
2025-05-10 15:25:23 +0100

    Add files via upload

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,23 +1,28 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("meu-cache-v1").then((cache) => {
-        return cache.addAll([
-          "/",
-          "/index.html",
-          "/style.css",
-          "/scripts/script.js",
-          "/scripts/fetch.js",
-          "/scripts/canvas.js"
-        ]);
+const CACHE_NAME = 'focusly-cache-v1';
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/scripts/script.js",
+  "/scripts/fetch.js",
+  "/scripts/canvas.js",
+  "letter-f-initial-icon-logo-template_23987-54-removebg-preview.png"
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((resposta) => {
-        return resposta || fetch(event.request);
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
       })
-    );
-  });
-  
+  );
+});
